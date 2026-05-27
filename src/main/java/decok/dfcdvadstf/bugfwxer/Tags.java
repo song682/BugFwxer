0a88b1e153a7f554a9d11cff47a7072997ea2348
song682 <songpengjie2007@163.com>
2026-05-27 23:22:14 +0800

    Fix directory

---
+++ src/main/java/decok/dfcdvadstf/bugfwxer/Tags.java
@@ -0,0 +1,6 @@
+package decok.dfcdvadstf.bugfwxer;
+public class Tags {
+    public static final String NAME = "BugFwxer";
+    public static final String MODID = "bugfwxer";
+    public static final String VERSION = "0.0.1";
+}
